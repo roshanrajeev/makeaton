747ba8b5d280f02784d0917274521e22aca2d131
Roshan R <roshanrajeev2001@gmail.com>
2022-10-27 17:08:26 +0530

    Format Errors

--- makeaton/settings.py
+++ makeaton/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'EXCEPTION_HANDLER': 'api.errors.exception_errors_format_handler',
+}
